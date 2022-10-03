8cdddc4c9bc25299188786c04f095239909751b4
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-10-03 11:55:35 -0400

    first card flipped

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-// Global Variables
+//////////////////// Global Variables////////////////////
 
-//Event Listeners
+const card = document.querySelector('.card')
+const front = document.querySelector('.front')
+const back = document.querySelector('.back')
+// let cardFront = document.querySelectorAll('.front')
+// let cardBack = document.querySelectorAll('.back')
+// let cardRed = document
 
-//Game Functions
+let changeDisplay = () => {
+  back.style.display = 'none'
+  front.style.display = 'block'
+}
+
+//////////////Event Listeners////////////////
+
+//will need to change this once figure out how to make .back display instead of .card in CSS
+card.addEventListener('click', changeDisplay)
+
+// card.addEventListener('click')
+
+// //adding class of .flipped to card that is clicked
+// card.addEventListener('click', document.classList.add('.flippped'))
+//will later need to remove .flipped class?
+
+/////////////////////Game Functions////////////////
